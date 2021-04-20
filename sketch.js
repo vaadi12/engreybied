1403a7c6bac6cd6e25199e075b84f9f9f26a7d85
vaadi12 <GMCHHARA@GMAIL.COM>
2021-04-20 18:47:53 +0530

    rename

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg;
+var b2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +18,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    b2 = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,6 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+   
 
 }
 
@@ -60,5 +62,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+   b2.display();
 }
